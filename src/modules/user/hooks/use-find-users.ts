9edabb00cec468a0dd0860e9a3536ccd383f9ca3
Bruno Murer <brudoburer@gmail.com>
2025-12-21 19:09:04 -0300

    chore: remove unused code

--- src/modules/user/hooks/use-find-users.ts
+++ src/modules/user/hooks/use-find-users.ts
@@ -1,8 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import { userApi } from '../utils/constants'
-import type { PaginatedParams } from '@/modules/common/api/api-types'
+import type { PaginatedParams } from '@/common/api/api-types'
 
-export const useFindUsers = ({ page, limit = 10, sort, sortOrder }: PaginatedParams) => {
+export const useFindUsers = ({
+  page,
+  limit = 10,
+  sort,
+  sortOrder,
+}: PaginatedParams) => {
   return useQuery({
     queryFn: () => userApi.find({ page, limit, sort, sortOrder }),
     queryKey: userApi.keys.find({ page, limit, sort, sortOrder }),
--- src/routes/admin/users/index.tsx
+++ src/routes/admin/users/index.tsx
@@ -1,14 +1,6 @@
 import { UsersPage } from '@/modules/user/pages/users-page'
 import { createFileRoute } from '@tanstack/react-router'
-import { z } from 'zod'
-
-const usersSearchSchema = z.object({
-  page: z.coerce.number().optional().default(1),
-  sort: z.string().optional(),
-  sortOrder: z.enum(['asc', 'desc']).optional(),
-})
 
 export const Route = createFileRoute('/admin/users/')({
   component: UsersPage,
-  validateSearch: usersSearchSchema,
 })
